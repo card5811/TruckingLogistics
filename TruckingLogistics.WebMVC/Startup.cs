95e744b62214e4666a313fbded7239297850aa2e
craig dunlap <cadunlap3234@gmail.com>
2020-04-09 12:07:36 -0400

    Add project files.

---
+++ TruckingLogistics.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TruckingLogistics.WebMVC.Startup))]
+namespace TruckingLogistics.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
